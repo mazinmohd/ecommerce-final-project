cea772a95a1f12f3f9651ac56274f816b6a75b0e
mazinmohd <mazenomermazen2002@gmail.com>
2026-06-10 08:05:20 +0200

    add cart page

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -28,3 +28,140 @@ function filterProducts(category){
     });
 
 }
+function addToCart(name, price){
+
+    let cart =
+    JSON.parse(
+    localStorage.getItem('cart')
+    ) || [];
+
+    cart.push({
+        name:name,
+        price:price,
+        quantity:1
+    });
+
+    localStorage.setItem(
+        'cart',
+        JSON.stringify(cart)
+    );
+
+    alert(name + " added to cart!");
+}
+
+function displayCart(){
+
+    let cart =
+    JSON.parse(
+    localStorage.getItem('cart')
+    ) || [];
+
+    const cartContainer =
+    document.getElementById('cart-items');
+
+    if(!cartContainer) return;
+
+    cartContainer.innerHTML = '';
+
+    let total = 0;
+
+    cart.forEach((item,index)=>{
+
+        total +=
+        item.price *
+        item.quantity;
+
+        cartContainer.innerHTML += `
+        <div class="cart-item">
+
+            <div class="cart-info">
+                <h3>${item.name}</h3>
+
+                <p>Price: $${item.price}</p>
+
+                <p>Quantity: ${item.quantity}</p>
+
+                <p>
+                    Subtotal:
+                    $${item.price * item.quantity}
+                </p>
+            </div>
+
+            <div class="cart-actions">
+
+                <button onclick="increaseQuantity(${index})">
+                    +
+                </button>
+
+                <button onclick="decreaseQuantity(${index})">
+                    -
+                </button>
+
+                <button onclick="removeItem(${index})">
+                    Remove
+                </button>
+
+            </div>
+
+        </div>
+        `;
+    });
+
+    document.getElementById(
+    'cart-total'
+    ).innerText =
+    "Total: $" + total;
+}
+function increaseQuantity(index){
+
+    let cart =
+    JSON.parse(
+    localStorage.getItem('cart')
+    );
+
+    cart[index].quantity++;
+
+    localStorage.setItem(
+        'cart',
+        JSON.stringify(cart)
+    );
+
+    displayCart();
+}
+function decreaseQuantity(index){
+
+    let cart =
+    JSON.parse(
+    localStorage.getItem('cart')
+    );
+
+    if(
+        cart[index].quantity > 1
+    ){
+        cart[index].quantity--;
+    }
+
+    localStorage.setItem(
+        'cart',
+        JSON.stringify(cart)
+    );
+
+    displayCart();
+}
+function removeItem(index){
+
+    let cart =
+    JSON.parse(
+    localStorage.getItem('cart')
+    );
+
+    cart.splice(index,1);
+
+    localStorage.setItem(
+        'cart',
+        JSON.stringify(cart)
+    );
+
+    displayCart();
+}
+displayCart();
