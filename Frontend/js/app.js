41bb311e87c6bd392596ab54b96c2609d41e5bc2
mazinmohd <mazenomermazen2002@gmail.com>
2026-06-10 06:57:03 +0200

    create products page

---
+++ Frontend/js/app.js
@@ -0,0 +1,30 @@
+function filterProducts(category){
+
+    const products =
+    document.querySelectorAll('.product-card');
+
+    products.forEach(product => {
+
+        if(category === 'all'){
+
+            product.style.display = 'block';
+
+        }
+
+        else if(
+            product.classList.contains(category)
+        ){
+
+            product.style.display = 'block';
+
+        }
+
+        else{
+
+            product.style.display = 'none';
+
+        }
+
+    });
+
+}
